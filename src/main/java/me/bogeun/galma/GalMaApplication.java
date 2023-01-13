d0f7c7b6077c9a34a2fdb0409857246f0ff524e1
idiot2222 <pky2892@gmail.com>
2023-01-13 10:58:40 +0900

    init project

---
+++ src/main/java/me/bogeun/galma/GalMaApplication.java
@@ -0,0 +1,13 @@
+package me.bogeun.galma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalMaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalMaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bogeun/galma/GalMaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bogeun.galma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalMaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
