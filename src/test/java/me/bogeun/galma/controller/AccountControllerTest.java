51455bf7e228ad3e064d55de6e835846dad333ad
idiot2222 <pky2892@gmail.com>
2023-01-15 20:41:14 +0900

    test: 회원 가입 테스트

--- src/test/java/me/bogeun/galma/controller/AccountControllerTest.java
+++ src/test/java/me/bogeun/galma/controller/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package me.bogeun.galma.controller;
 
+import me.bogeun.galma.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,6 +24,29 @@ class AccountControllerTest {
     @DisplayName("GET 회원 가입 페이지")
     void getSignUp() throws Exception {
         mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpDto"));
+    }
+
+    @Test
+    @DisplayName("POST 회원 가입 기능 - 성공")
+    void postSignUpSuccess() throws Exception {
+        mockMvc.perform(post("/sign-up").with(csrf())
+                        .param("username", "test_username")
+                        .param("password", "test_password")
+                        .param("email", "test@email.com"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/login"));
+    }
+
+    @Test
+    @DisplayName("POST 회원 가입 기능 - 실패")
+    void postSignUpFail() throws Exception {
+        mockMvc.perform(post("/sign-up").with(csrf())
+                        .param("username", "**")
+                        .param("password", "password")
+                        .param("email", "123"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"));
     }
