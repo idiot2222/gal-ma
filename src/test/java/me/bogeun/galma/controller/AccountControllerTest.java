669d3e1b63e0b9e12131f6addd31c8bc12f39968
idiot2222 <pky2892@gmail.com>
2023-01-17 19:03:25 +0900

    test: 로그인 관련 테스트

--- src/test/java/me/bogeun/galma/controller/AccountControllerTest.java
+++ src/test/java/me/bogeun/galma/controller/AccountControllerTest.java
@@ -1,23 +1,44 @@
 package me.bogeun.galma.controller;
 
+import me.bogeun.galma.dto.SignUpDto;
+import me.bogeun.galma.service.AccountService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+
+    @BeforeEach
+    void beforeEach() {
+        SignUpDto signUpDto = new SignUpDto();
+        signUpDto.setUsername("username");
+        signUpDto.setPassword("password123");
+        signUpDto.setEmail("user@email.com");
+
+        accountService.signUp(signUpDto);
+    }
+
 
     @Test
     @DisplayName("GET 회원 가입 페이지")
@@ -56,5 +77,26 @@ class AccountControllerTest {
         mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/login"));
+    }
+
+    @Test
+    @DisplayName("POST 로그인 - 성공")
+    void postLoginSuccess() throws Exception {
+        mockMvc.perform(post("/login").with(csrf())
+                        .param("username", "username")
+                        .param("password", "password123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("username"));
+    }
+
+    @Test
+    @DisplayName("POST 로그인 - 실패")
+    void postLoginFail() throws Exception {
+        mockMvc.perform(post("/login").with(csrf())
+                        .param("username", "wrong_username")
+                        .param("password", "password123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
     }
 }
