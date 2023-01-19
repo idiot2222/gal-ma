6a4d56f7e24e8887fc3c4dde7bedc5504c3a7985
idiot2222 <pky2892@gmail.com>
2023-01-19 15:02:49 +0900

    feature: Global Exception Handler + error page 생성

---
+++ src/main/java/me/bogeun/galma/controller/ErrorController.java
@@ -0,0 +1,16 @@
+package me.bogeun.galma.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleError(Exception e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+
+        return "error";
+    }
+}
