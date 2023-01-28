75b0841303a73150bf3a5752227131fc87219c18
idiot2222 <pky2892@gmail.com>
2023-01-29 00:27:54 +0900

    feature: 로고랑 파비콘 추가
    Signed-off-by: idiot2222 <pky2892@gmail.com>

--- src/main/java/me/bogeun/galma/config/WebConfig.java
+++ src/main/java/me/bogeun/galma/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/");
-        registry.addResourceHandler("/favicon.ico")
+        registry.addResourceHandler("/static/favicon.ico")
                 .addResourceLocations("classpath:/static/");
     }
 }
