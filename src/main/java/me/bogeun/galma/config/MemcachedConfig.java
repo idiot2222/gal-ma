c64b1bbb37de2c132c233412e245b2effaf602de
idiot2222 <pky2892@gmail.com>
2023-02-20 03:02:10 +0900

    config: memcached 연결 설정 변경
    Signed-off-by: idiot2222 <pky2892@gmail.com>

--- src/main/java/me/bogeun/galma/config/MemcachedConfig.java
+++ src/main/java/me/bogeun/galma/config/MemcachedConfig.java
@@ -1,6 +1,7 @@
 package me.bogeun.galma.config;
 
 import net.spy.memcached.MemcachedClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,15 @@ import java.net.InetSocketAddress;
 @Configuration
 public class MemcachedConfig {
 
+    @Value("${api.memcached.host}")
+    private String host;
+
+    @Value("${api.memcached.port}")
+    private int port;
+
     @Bean
     public MemcachedClient memcachedClient() throws IOException {
-        return new MemcachedClient(new InetSocketAddress("localhost", 11211));
+        return new MemcachedClient(new InetSocketAddress(host, port));
     }
 
 }
