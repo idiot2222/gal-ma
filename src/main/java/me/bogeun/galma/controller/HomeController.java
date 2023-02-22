05ce967fa812258744c10d8cda60cebab4450771
idiot2222 <pky2892@gmail.com>
2023-02-22 11:06:27 +0900

    feature: 메인 페이지에 오늘 경기 정보 추가
    Signed-off-by: idiot2222 <pky2892@gmail.com>

--- src/main/java/me/bogeun/galma/controller/HomeController.java
+++ src/main/java/me/bogeun/galma/controller/HomeController.java
@@ -1,23 +1,37 @@
 package me.bogeun.galma.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.bogeun.galma.entity.Match;
 import me.bogeun.galma.payload.HomePostListDto;
+import me.bogeun.galma.payload.WeatherDto;
+import me.bogeun.galma.service.MatchService;
 import me.bogeun.galma.service.PostService;
+import me.bogeun.galma.utils.WeatherDataSetting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
     private final PostService postService;
+    private final MatchService matchService;
+
+    private final WeatherDataSetting weatherDataSetting;
 
     @GetMapping("/")
     public String getIndex(Model model) {
         HomePostListDto listDto = postService.getHomePostList();
+        Match todayMatch = matchService.getTodayMatch();
+
+        List<WeatherDto> weather = weatherDataSetting.getWeather(todayMatch.getMatchStadium());
 
         model.addAttribute("lists", listDto);
+        model.addAttribute("match", todayMatch);
+        model.addAttribute("weather", weather);
 
         return "index";
     }
